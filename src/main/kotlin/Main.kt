de6e1287400383af26d51e45cf7bc4fca1ce065e
unknown <sethmegbetor@gmail.com>
2021-03-12 06:20:38 +0000

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,56 @@
+fun main() {
+//println("Hello World")
+//    var x: Int = 3
+//    println("the value of x is: $x")
+//    x = 5
+//    println("the value of x is now: $x")
+
+//    numbers and variable declaration
+//    val x = 20
+//    val wholeNumber = 3
+//    val bigNumber = 3L
+//    val preciseDecimal = 3.33f
+//    val iLoveVegetables = false
+//    val iLovePizza = true
+//    val iAmALetter = 'a'
+//    print("the value of the variable x is $x")
+
+//    arithmetic operators
+//    val x = 3 + 4
+//    val floatNumbers = 5f / 6f
+//    println("the value of x is $x")
+//    println("the value of x is $floatNumbers")
+////    boolean
+//    val amIAnAdult = true
+//    val amIAProgrammer = true
+//    val amIAnAdultProgrammer = amIAnAdult && amIAProgrammer
+//    print("Am I an adult programmer is: $amIAnAdultProgrammer")
+
+//    strings
+//    val strings = "I am loving this language"
+//    println(strings)
+//    println(strings.toUpperCase())
+
+//    conditions
+//    val x = 1 + 3
+//    if (x == 2){
+//        println("x is 2")
+//    } else if(x == 3){
+//        println("x is 3")
+//    } else {
+//        println("this is just as awesome as it gets")
+//    }
+//    val y = if(x == 2)2 else 3
+
+//    null pointer exception
+//    type is not nullable by default. Either it is or it is not
+//    val x: Int? = null
+//    accepting user input
+//    val number1 = readLine() ?: "0"
+//    val number2 = readLine() ?: "0"
+//    val result =  number1.toInt() + number2.toInt()
+//    println(result)
+
+//    list: data containers we can use to store a bunch of variables
+
+}
